959e3df3a03b16f7de3f861773ad487693c0be5e
허성현 <96102917+hyeonhh@users.noreply.github.com>
2024-02-01 20:06:10 +0900

    Delete app/src/main/java/com/example/sejong2washertimer/api/ApiKey.kt

--- app/src/main/java/com/example/sejong2washertimer/api/ApiKey.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sejong2washertimer.api
-
-class ApiKey {
-    companion object{
-        const val API_KEY = "D5Pn1X94hE69T8eSXEWBopXajX0xhBgzDbAEkf5CCiJL4jp2I598D4ZCP9gNsnM8tRGYfL6hKiFC5KYccYVuSA%3D%3D"
-    }
-}
